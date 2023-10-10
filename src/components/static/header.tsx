4ca255e8545c2fb2222560a2d7ed90487d650b9d
Alonso-GarnierBBDO <artaviaalonso60@gmail.com>
2023-10-09 22:27:11 -0600

    Se inicio el ajuste del menu

--- src/components/static/header.tsx
+++ src/components/static/header.tsx
@@ -1,11 +1,26 @@
-
+import Link from 'next/link';
+import Logo from '@/assets/img/Green-and-gold.png';
+import Image from 'next/image';
 
 function Header(){
 
     return (
         <>
             <header>
-                Aqui va el menu
+                <Link href="/" title="Volver a inicio">
+                    <Image
+                        src={Logo}
+                        alt='Logo de Green and Gold'
+                    />
+                </Link>
+                <ul>
+                    <li>
+                        <Link href="/about-us">About Us</Link>
+                    </li>
+                    <li>
+                        <Link href="/services">Services</Link>
+                    </li>
+                </ul>
             </header>
         </>
     )
