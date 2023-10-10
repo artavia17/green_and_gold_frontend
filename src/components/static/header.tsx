be2dacad9f5a79c553cebfe0cc36807bda85b9e4
Alonso-GarnierBBDO <artaviaalonso60@gmail.com>
2023-10-10 08:57:10 -0600

    Se agrego el hover del menu

--- src/components/static/header.tsx
+++ src/components/static/header.tsx
@@ -1,8 +1,43 @@
+'use client';
+
 import Link from 'next/link';
 import Logo from '@/assets/img/Green-and-gold.png';
 import Image from 'next/image';
 
+import { useState } from 'react';
+
 function Header(){
+
+    const [activeMenu, setActiveMenu] = useState(false);
+    const [activeView, setActiveView] = useState(false);
+    const [activeRemove, setActiveRemove] = useState(false);
+
+    const handleMenuClick = () => {
+
+        let time = 0;
+
+        if(!activeMenu){
+            setActiveView(true);
+            setTimeout(() => {
+                activeMenu ? setActiveMenu(false) : setActiveMenu(true);
+            }, 10)
+        }else{
+            setActiveView(false);
+            setActiveRemove(true);
+
+            if(window.innerWidth <= 600){
+                time = 1000;
+            }else{
+                time = 500;
+            }
+            
+
+            setTimeout(() => {
+                activeMenu ? setActiveMenu(false) : setActiveMenu(true);
+                setActiveRemove(false);
+            }, time)
+        }
+    }
 
     return (
         <>
@@ -13,14 +48,22 @@ function Header(){
                         alt='Logo de Green and Gold'
                     />
                 </Link>
-                <ul>
-                    <li>
-                        <Link href="/about-us">About Us</Link>
-                    </li>
-                    <li>
-                        <Link href="/services">Services</Link>
-                    </li>
-                </ul>
+                <button className={`menu_item ${activeMenu ? 'active' : ''} ${activeRemove ? 'remove_menu_item' : ''}`} onClick={handleMenuClick}>
+                    <hr />
+                    <hr />
+                    <hr />
+                </button>
+                <div className={`content_menu ${activeMenu ? 'active' : ''} ${activeView ? 'view' : ''} ${activeRemove ? 'remove' : ''}`}>
+                    <div className="capa_close" onClick={handleMenuClick}></div>
+                    <ul>
+                        <li>
+                            <Link onClick={handleMenuClick} href="/about-us">About Us</Link>
+                        </li>
+                        <li>
+                            <Link onClick={handleMenuClick} href="/services">Services</Link>
+                        </li>
+                    </ul>
+                </div>
             </header>
         </>
     )
