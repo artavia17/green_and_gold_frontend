b725d3c3618f905653108df185bb6d336ccee0f1
Alonso-GarnierBBDO <artaviaalonso60@gmail.com>
2023-10-09 21:04:55 -0600

    Agregamos la ruta de servicios

---
+++ src/app/services/page.tsx
@@ -0,0 +1,17 @@
+/**
+ * Desde este archivo enviamos mostramos el contenido de la seccion de servicios
+ */
+
+function Services(){
+
+    return (
+        <>
+
+            <h1>Servicios</h1>
+        
+        </>
+    )   
+
+}
+
+export default Services;
