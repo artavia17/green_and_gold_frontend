b1b86fe7985fefeb5af2d7e50ad4177b64a1e6ec
Alonso-GarnierBBDO <artaviaalonso60@gmail.com>
2023-10-10 21:17:25 -0600

    Se agrego el slider del home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-
+import SliderHome from "@/components/home/slider"
 
 export default function Home() {
   return (
     <>
-      <h1>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor </h1>
+      <SliderHome/>
     </>
   )
 }
---
+++ src/components/home/slider.tsx
@@ -0,0 +1,95 @@
+'use client';
+
+// Aqui vamos a utilizar la libreria de swiper
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, EffectCoverflow, Autoplay } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/scss/navigation';
+
+// Imagenes de prueba
+import ImageBackgroundTest from '@/assets/img/test/background_swiper_test.png';
+import ImageBackgroundTestTwo from '@/assets/img/test/two.jpg';
+
+
+function SliderHome(){
+
+    const allSlider = [
+        {
+            title: 'Looking for your dream vacation',
+            textButton: 'Check our rental homes',
+            linkButton: 'https://swiperjs.com/react',
+            img: ImageBackgroundTest.src,
+            newTarget: false,
+        },
+        {
+            title: 'Looking for your dream vacation',
+            textButton: 'Check our rental homes',
+            linkButton: 'https://swiperjs.com/react',
+            img: ImageBackgroundTestTwo.src,
+            newTarget: true
+        },
+        {
+            title: 'Looking for your dream vacation',
+            textButton: 'Check our rental homes',
+            linkButton: 'https://swiperjs.com/react',
+            img: ImageBackgroundTest.src,
+            newTarget: false
+        },
+        {
+            title: 'Looking for your dream vacation',
+            textButton: 'Check our rental homes',
+            linkButton: 'https://swiperjs.com/react',
+            img: ImageBackgroundTestTwo.src,
+            newTarget: true
+        },
+    ]
+
+
+    return (
+        <>
+            
+            <section className='sliderHome'>
+                <Swiper
+                    modules={[Pagination, EffectCoverflow, Autoplay]}
+                    grabCursor={true}
+                    spaceBetween={50}
+                    slidesPerView={1}
+                    parallax={true}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    centeredSlides={true}
+                    effect="coverflow"
+                    autoplay={{
+                        delay: 3000,
+                    }}
+                    onSlideChange={() => console.log('slide change')}
+                    onSwiper={(swiper) => console.log(swiper)}
+                >
+                    {
+                        allSlider.map( (item, key) =>{
+                            return (
+                                <SwiperSlide 
+                                    className='item' 
+                                    key={key}
+                                    style={{
+                                        backgroundImage: `url(${item.img})`,
+                                    }}
+                                >
+                                    <section className='content'>
+                                        <h2>{item.title}</h2>
+                                        <a href={item.linkButton} target={item.newTarget ? '_blank' : ''}>{item.textButton}</a>
+                                    </section>
+                                </SwiperSlide>
+                            )
+                        })
+                    }
+                </Swiper>
+            </section>  
+
+        </>
+    )
+
+}
+
+export default SliderHome;
