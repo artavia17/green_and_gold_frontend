18fa82cdcd8892dad647781b4b9bee1508cffea8
Alonso-GarnierBBDO <artaviaalonso60@gmail.com>
2023-11-06 10:10:56 -0600

    Se configuro el build de react

--- next.config.js
+++ next.config.js
@@ -3,11 +3,12 @@
 const path = require('path')
 
 module.exports = { 
-    output: 'export',
+    // output: 'export',
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
     },
     images: {
-        unoptimized: true
+        unoptimized: false
     }
 }
+
---
+++ src/app/rental-home/[slug]/page.tsx
@@ -0,0 +1,12 @@
+
+function SlugPage(){
+
+    return (
+        <>
+            <h1>Hola mudo </h1>
+        </>
+    )
+
+}
+
+export default SlugPage;
